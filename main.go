bd2108737522467eacfdced7f1e7119f38b690e6
Brandon Sprague <brandon00sprague@gmail.com>
2017-10-12 20:16:45 -0700

    Start building the web serving portion

--- main.go
+++ main.go
@@ -1,27 +1,25 @@
 package main
 
 import (
-	"bufio"
 	"flag"
-	"fmt"
 	"io/ioutil"
 	"log"
+	"net/http"
 	"os"
 
-	"github.com/bcspragu/Codenames/codenames"
 	"github.com/bcspragu/Codenames/dict"
-	"github.com/bcspragu/Codenames/types"
+	"github.com/bcspragu/Codenames/server"
 	"github.com/bcspragu/Codenames/vision"
-	"github.com/bcspragu/Codenames/w2v"
 	"golang.org/x/net/context"
 	"google.golang.org/api/option"
 )
 
 func main() {
 	var (
+		addr       = flag.String("addr", ":8080", "The port to run the web server on.")
 		apiKeyFile = flag.String("api_key_file", "", "The file containing an API key to authenticate with Google Cloud")
 		dictFile   = flag.String("dict_file", "words.txt", "A newline-separated dictionary of words, all in uppercase letters.")
-		modelFile  = flag.String("model_file", "w2v.bin", "A binary-formatted word2vec pre-trained model file.")
+		//modelFile  = flag.String("model_file", "w2v.bin", "A binary-formatted word2vec pre-trained model file.")
 	)
 	flag.Parse()
 
@@ -51,39 +49,35 @@ func main() {
 	}
 
 	// Initialize our word2vec model.
-	if err := w2v.Init(*modelFile); err != nil {
-		log.Fatalf("Failed to initialize word2vec model: %v", err)
-	}
-
-	f, err := os.Open("codenames.jpg")
+	//if err := w2v.Init(*modelFile); err != nil {
+	//log.Fatalf("Failed to initialize word2vec model: %v", err)
+	//}
+	srv, err := server.New(cvtr)
 	if err != nil {
-		log.Fatalf("Failed to open new image: %v", err)
+		log.Fatalf("Failed to initialize server: %v", err)
 	}
-	defer f.Close()
+	http.ListenAndServe(*addr, srv)
 
-	b, err := cvtr.BoardFromReader(ctx, f)
-	if err != nil {
-		log.Fatalf("Failed to generate board: %v", err)
-	}
-
-	g, err := codenames.NewGame(b, &codenames.Config{
-		Team: types.RedTeam,
-		Role: types.Operative,
-	})
-	if err != nil {
-		log.Fatalf("Failed to instantiate game: %v", err)
-	}
-
-	sc := bufio.NewScanner(os.Stdin)
-	fmt.Println("Game started, enter clue: ")
-	for sc.Scan() {
-		guesses, err := g.Guess(sc.Text())
+	/*
+		g, err := codenames.NewGame(b, &codenames.Config{
+			Team: types.RedTeam,
+			Role: types.Operative,
+		})
 		if err != nil {
-			log.Printf("Failed to guess: %v", err)
+			log.Fatalf("Failed to instantiate game: %v", err)
 		}
-		for i, g := range guesses {
-			fmt.Printf("Guess #%d = %q\n", i, g)
+
+		sc := bufio.NewScanner(os.Stdin)
+		fmt.Println("Game started, enter clue: ")
+		for sc.Scan() {
+			guesses, err := g.Guess(sc.Text())
+			if err != nil {
+				log.Printf("Failed to guess: %v", err)
+			}
+			for i, g := range guesses {
+				fmt.Printf("Guess #%d = %q\n", i, g)
+			}
+			fmt.Println("Board loaded, enter clue: ")
 		}
-		fmt.Println("Board loaded, enter clue: ")
-	}
+	*/
 }
