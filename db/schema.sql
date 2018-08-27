04adbf91e6553b9e0d454218414ae88021bceb6d
Mike Bjorge <mike.bjorge@gmail.com>
2018-08-26 19:34:27 -0700

    Add some comments to the schema to make things extra clear.

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,12 @@
 CREATE TABLE Users (
-    user_id TEXT,
-    display_name TEXT,
+    user_id TEXT,  -- Based on the user's cookie
+    display_name TEXT,  -- Arbitary, user specified
     PRIMARY KEY (user_id)
 );
 
 CREATE TABLE Games (
-    game_id TEXT,
-    status TEXT,
+    game_id TEXT,  -- Pronounceable, random combo of words
+    status TEXT,  -- Enum: PENDING, PLAYING, FINISHED
     state BLOB,
     PRIMARY KEY (game_id)
 );
@@ -14,8 +14,8 @@ CREATE TABLE Games (
 CREATE TABLE GamePlayers (
     game_id TEXT,
     user_id TEXT,
-    role TEXT,
-    team TEXT,
+    role TEXT,  -- Enum: SPYMASTER, OPERATIVE
+    team TEXT,  -- Enum: RED, BLUE
     FOREIGN KEY (game_id) REFERENCES Games(game_id),
     FOREIGN KEY (user_id) REFERENCES Users(user_id),
     PRIMARY KEY (game_id, user_id)
