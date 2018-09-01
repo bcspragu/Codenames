9c6f80feb541f92c8f8af1b8a9ae8daf2796fe98
Brandon Sprague <brandon00sprague@gmail.com>
2018-09-01 10:38:45 -0700

    Fix schema

--- sqldb/schema.sql
+++ sqldb/schema.sql
@@ -1,14 +1,14 @@
 CREATE TABLE Users (
     id TEXT NOT NULL,  -- Based on the user's cookie
     display_name TEXT NOT NULL,  -- Arbitary, user specified
-    PRIMARY KEY (user_id)
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE Games (
     id TEXT NOT NULL,  -- "Pronounceable", random combo of words
     status TEXT NOT NULL,  -- Enum: PENDING, PLAYING, FINISHED
     state BLOB NOT NULL,
-    PRIMARY KEY (game_id)
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE GamePlayers (
@@ -16,8 +16,8 @@ CREATE TABLE GamePlayers (
     user_id TEXT NOT NULL,
     role TEXT NOT NULL,  -- Enum: SPYMASTER, OPERATIVE
     team TEXT NOT NULL,  -- Enum: RED, BLUE
-    FOREIGN KEY (game_id) REFERENCES Games(game_id),
-    FOREIGN KEY (user_id) REFERENCES Users(user_id),
+    FOREIGN KEY (game_id) REFERENCES Games(id),
+    FOREIGN KEY (user_id) REFERENCES Users(id),
     PRIMARY KEY (game_id, user_id)
 );
 
@@ -25,6 +25,6 @@ CREATE TABLE GameHistory (
     game_id TEXT NOT NULL,
     event_timestamp DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
     event BLOB NOT NULL,
-    FOREIGN KEY (game_id) REFERENCES Games(game_id),
+    FOREIGN KEY (game_id) REFERENCES Games(id),
     PRIMARY KEY (game_id, event_timestamp)
 );
