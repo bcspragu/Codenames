5a60d5cf01fa3932cadad231081144b1805f14bc
Brandon Sprague <brandon00sprague@gmail.com>
2018-09-01 09:20:31 -0700

    Revert "Trying out the frontend autodeploy"
    This reverts commit e1909cc3fe6c82ed817f59b4701257ad8207057e.
    
    It's mainly meant to be a test of how much a warm cache speeds up
    deployments.

--- web/frontend/src/views/Home.vue
+++ web/frontend/src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Hrhr. Butts."/>
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
 </template>
 
