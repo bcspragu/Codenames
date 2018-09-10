29e094307ccb066dbf0ecc35a0480fd5fe1dae91
Brandon Sprague <brandon00sprague@gmail.com>
2018-09-09 17:54:54 -0700

    Add some debug lines, to determine if I'm going insane or not

--- cmd/codenames-server/main.go
+++ cmd/codenames-server/main.go
@@ -23,6 +23,9 @@ func main() {
 
 	flag.Parse()
 
+	log.Printf("HTTP Address: %q", *addr)
+	log.Printf("DB Path: %q", *dbPath)
+
 	r := rand.New(cryptoRandSource{})
 	db, err := sqldb.New(*dbPath, r)
 	if err != nil {
