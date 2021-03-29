88a590db298034d2ca56708c5989f7debb9b57f4
Brian Wong <brianwong.se@gmail.com>
2021-03-28 21:47:05 -0700

    Add new web frontend bootstrapped with Next.JS + TypeScript. Begin WebSocket foundation.

---
+++ web/frontend/api/websocket/index.ts
@@ -0,0 +1,42 @@
+const areWebSocketsSupported = () => {
+  return (
+    typeof window !== 'undefined' && typeof window.WebSocket !== 'undefined'
+  );
+};
+
+export class Socket {
+  connection?: WebSocket;
+
+  constructor() {
+    if (!areWebSocketsSupported()) {
+      return; // Hopefully just a server-side render...
+    }
+
+    this.connect();
+  }
+
+  connect() {
+    const uri = this.getURI();
+
+    this.connection = new WebSocket(uri);
+    this.connection.onmessage = this.handleMessage;
+    this.connection.onclose = this.handleClose;
+  }
+
+  getURI() {
+    const { location } = window;
+    const wsProtocol = location.protocol === 'https:' ? 'wss:' : 'ws:';
+    const wsGameEndpoint = '/api/ws/game/:id';
+
+    return `${wsProtocol}//${location.host}${wsGameEndpoint}`;
+  }
+
+  handleMessage = (ev) => {
+    // this.update(ev);
+    console.log('message event', ev);
+  };
+
+  handleClose = (ev) => {
+    console.log('onclose event', ev);
+  };
+}
