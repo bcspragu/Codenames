f59a4a409f1c0b910101f5adbdc8150ea2a79034
Brian Wong <brianwong.se@gmail.com>
2021-05-27 21:57:36 -0700

    Start home/root/game creation page

--- web/frontend/pages/index.tsx
+++ web/frontend/pages/index.tsx
@@ -1,18 +1,53 @@
 import Link from 'next/link';
+import { useEffect, useState } from 'react';
+import { call } from '../utils/call';
 
 export default function Home() {
+  const [name, setName] = useState('');
+  const [wasLoggedIn, setWasLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const initState = async () => {
+      const res = await call('GET', '/api/user');
+      console.log(res);
+
+      if (res && res.name) {
+        setName(res.name);
+        setWasLoggedIn(true);
+      }
+    };
+
+    initState();
+  }, [null]);
+
+  const handleCreateNewGame = async () => {
+    if (wasLoggedIn) {
+      await call('PATCH', '/api/user', { name: name.trim() });
+    } else {
+      await call('POST', '/api/user', { name: name.trim() });
+    }
+
+    const res = await call('POST', '/api/game');
+
+    console.log(res); // TODO put the gameId in store, ideally
+  };
+
   return (
     <div>
       <h1>codenames.ai</h1>
-      Go to{' '}
-      <Link href='/sandbox'>
-        <a>/sandbox</a>
-      </Link>{' '}
-      or{' '}
-      <Link href='/game/someBogusId'>
-        <a>/game/[whateverIdForNow]</a>
-      </Link>{' '}
-      to explore
+      <div className='tile'>
+        <form action='submit' onSubmit={(e) => e.preventDefault()}>
+          <input
+            type='text'
+            placeholder='Name'
+            value={name}
+            onChange={(e) => setName(e.target.value.trimStart())}
+          />
+          <button type='submit' onClick={handleCreateNewGame}>
+            Create new game
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
--- web/frontend/pages/sandbox.tsx
+++ web/frontend/pages/sandbox.tsx
@@ -1,21 +1,5 @@
 import { useState } from 'react';
-
-type Method = 'GET' | 'POST';
-
-const call = async (method: Method, endpoint: string, body?: any) => {
-  const res = await fetch(endpoint, { method, body: JSON.stringify(body) });
-  const contentType = res.headers.get('content-type');
-
-  if (contentType?.includes('application/json')) {
-    return res.json();
-  }
-
-  if (contentType?.includes('text/html')) {
-    return res.text();
-  }
-
-  return res;
-};
+import { call, Method } from '../utils/call';
 
 export default function Sandbox() {
   const [method, setMethod] = useState<Method>('GET');
